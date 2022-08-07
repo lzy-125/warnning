6c8fee83aa6e4d383eab96bb0be454991fd5d89a
andilylzy <445687557@qq.com>
2022-08-07 13:53:40 +0800

    解决前后端跨域问题

--- chat/src/main/java/com/example/chat/controller/MessageController.java
+++ chat/src/main/java/com/example/chat/controller/MessageController.java
@@ -7,9 +7,9 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-
-@RestController
+@CrossOrigin
 @Log4j2
+@RestController
 public class MessageController {
 
     @Autowired
